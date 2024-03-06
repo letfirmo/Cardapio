f62adc86eb1483941fb9d509cf928b461b2bff61
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-03-05 21:40:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import hashtaurante from "./assets/hashtaurante.webp"
+import './App.css'
+
+export function App() {
+  return <>
+  <img src={hashtaurante} alt="varanda de um restaurante com mesas postas" className="capa"/>
+  <h1>Cardápio</h1>
+  </>
+}
