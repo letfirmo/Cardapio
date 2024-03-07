6fd8091d76fb0d57a1b769dc60c1fdac6800fdb3
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-03-06 22:04:12 -0300

    01:29:19

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import hashtaurante from "./assets/hashtaurante.webp"
 import './App.css'
+import { Navegacao } from "./Navegacao.jsx"
+import { ItemCardapio } from "./ItemCardapio.jsx"
 
 export function App() {
   return <>
-  <img src={hashtaurante} alt="varanda de um restaurante com mesas postas" className="capa"/>
-  <h1>Cardápio</h1>
+    <img src={hashtaurante} alt="varanda de um restaurante com mesas postas" className="capa"/>
+    <Navegacao />
+    <ItemCardapio />
+    <ItemCardapio />
+    <ItemCardapio />
+    <ItemCardapio />
   </>
 }
