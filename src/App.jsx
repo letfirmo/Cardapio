e0929eee06d8ef4172cb901629ec9cb3f380ffac
Leticia Oliveira <leticiasilvaoliveira14@gmail.com>
2024-03-08 20:16:03 -0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react"
 import hashtaurante from "./assets/hashtaurante.webp"
 import './App.css'
 import { Navegacao } from "./Navegacao.jsx"
 import { ItemCardapio } from "./ItemCardapio.jsx"
+import { pratosPrincipais, sobremesas, bebidas } from "./cardapio.js"
+
 
 export function App() {
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas]
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0)
+  
   return <>
     <img src={hashtaurante} alt="varanda de um restaurante com mesas postas" className="capa"/>
-    <Navegacao />
-    <ItemCardapio />
-    <ItemCardapio />
-    <ItemCardapio />
-    <ItemCardapio />
+    <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada} />
+    <div className="menu">
+      {paginasMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem} />)}
+    </div>
   </>
 }
